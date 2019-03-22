684e6f1abc40c43191c2f39d9cb89cefa7c93a70
root <dominik.obuch@wp.pl>
2019-03-22 15:24:00 +0000

    gh-pages fix

--- app/js/hamburger.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("hamburger").addEventListener("click", function () {
-    for (let i = 0; i < document.getElementsByClassName("hamburger").length; i++) {
-        document.getElementsByClassName("hamburger")[i].classList.toggle("hamburger__expanded");
-    }
-})
